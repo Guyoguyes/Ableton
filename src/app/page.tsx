fa7b5fcf983e6f3ccf5d98a55bb02f7b20b83ae7
Guyoguyes <g.abduba43@gmail.com>
2023-08-14 22:12:17 +0300

    added hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
 
 export default function Home() {
   return (
-    <div className="container px-auto">
+    <div className="px-auto w-full">
       <Navbar />
+      <Hero />
     </div>
   )
 }
